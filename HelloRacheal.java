ba36019cfd67e4997b4ac1ec672a8c7648529073
Valentine Ofili <valentine.ofili@gmail.com>
2020-03-25 15:14:03 +0100

    Added changes more chnages to file

--- HelloRacheal.java
+++ HelloRacheal.java
@@ -7,6 +7,7 @@ public class HelloRacheal {
         System.out.println("This is a new Change from Developer");
         System.out.println("File is ready for build");
         System.out.println("Trigger another Build ");
+        System.out.println("Add this after 2 minutes");
         
     }
     
