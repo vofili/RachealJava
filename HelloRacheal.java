679d16210f8e07bb6ee46822e2a7b16767a1de88
Valentine Ofili <valentine.ofili@gmail.com>
2020-03-25 13:30:00 +0100

    Racheal Init Commit

---
+++ HelloRacheal.java
@@ -0,0 +1,10 @@
+
+public class HelloRacheal {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello Racheal im Compiling with Jenkins");
+        
+    }
+    
+}
