3aa3b647e5b81d529112787e3e4deb79e48f3ccc
Valentine Ofili <valentine.ofili@gmail.com>
2020-03-25 14:50:29 +0100

    Added changes to file

--- HelloRacheal.java
+++ HelloRacheal.java
@@ -4,6 +4,8 @@ public class HelloRacheal {
     
     public static void main(String[] args) {
         System.out.println("Hello Racheal im Compiling with Jenkins");
+        System.out.println("This is a new Change from Developer");
+        System.out.println("File is ready for build");
         
     }
     
