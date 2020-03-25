2ec084f413914e69a1a327cabaa673c15c5c8270
Valentine Ofili <valentine.ofili@gmail.com>
2020-03-25 15:17:47 +0100

    Added chewing gum to my code

--- HelloRacheal.java
+++ HelloRacheal.java
@@ -3,11 +3,7 @@ public class HelloRacheal {
 
     
     public static void main(String[] args) {
-        System.out.println("Hello Racheal im Compiling with Jenkins");
-        System.out.println("This is a new Change from Developer");
-        System.out.println("File is ready for build");
-        System.out.println("Trigger another Build ");
-        System.out.println("Add this after 2 minutes");
+        System.out.println("I'm tired and want to chew Gum");
         
     }
     
